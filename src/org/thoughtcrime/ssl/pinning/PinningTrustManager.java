06a9f9e7a709ff3d07aca422eb3ecdce21a97e51
olivierg13 <olivier.goutay@gmail.com>
2016-05-06 09:52:54 -0700

    Adding chain logging on CertificateException

--- src/org/thoughtcrime/ssl/pinning/PinningTrustManager.java
+++ src/org/thoughtcrime/ssl/pinning/PinningTrustManager.java
@@ -1,16 +1,16 @@
 /**
  * Copyright (C) 2011-2013 Moxie Marlinspike
- *
+ * <p/>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p/>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * <p/>
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -30,13 +30,14 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
 
 /**
  * A TrustManager implementation that enforces Certificate "pins."
- *
+ * <p/>
  * <p>
  * PinningTrustManager is layered on top of the system's default TrustManager,
  * such that the system continues to validate CA signatures for SSL connections
@@ -66,134 +67,151 @@ import javax.net.ssl.X509TrustManager;
  */
 public class PinningTrustManager implements X509TrustManager {
 
-  private final TrustManager[] systemTrustManagers;
-  private final SystemKeyStore systemKeyStore;
-  private final long           enforceUntilTimestampMillis;
+    private final TrustManager[] systemTrustManagers;
+    private final SystemKeyStore systemKeyStore;
+    private final long enforceUntilTimestampMillis;
 
-  private final List<byte[]> pins          = new LinkedList<byte[]>();
-  private final Set<X509Certificate> cache = Collections.synchronizedSet(new HashSet<X509Certificate>());
+    private final List<byte[]> pins = new LinkedList<byte[]>();
+    private final Set<X509Certificate> cache = Collections.synchronizedSet(new HashSet<X509Certificate>());
 
-  /**
-   * Constructs a PinningTrustManager with a set of valid pins.
-   *
-   * @param keyStore A SystemKeyStore that validation will be based on.
-   *
-   * @param pins An array of encoded pins to match a seen certificate
-   *             chain against. A pin is a hex-encoded hash of a X.509 certificate's
-   *             SubjectPublicKeyInfo. A pin can be generated using the provided pin.py
-   *             script: python ./tools/pin.py certificate_file.pem
-   *
-   * @param enforceUntilTimestampMillis A timestamp (in milliseconds) when pins will stop being
-   *                                    enforced.  Normal non-pinned certificate validation
-   *                                    will continue.  Set this to some period after your build
-   *                                    date, or to 0 to enforce pins forever.
-   */
-  public PinningTrustManager(SystemKeyStore keyStore, String[] pins, long enforceUntilTimestampMillis) {
-    this.systemTrustManagers         = initializeSystemTrustManagers(keyStore);
-    this.systemKeyStore              = keyStore;
-    this.enforceUntilTimestampMillis = enforceUntilTimestampMillis;
+    /**
+     * Constructs a PinningTrustManager with a set of valid pins.
+     *
+     * @param keyStore                    A SystemKeyStore that validation will be based on.
+     * @param pins                        An array of encoded pins to match a seen certificate
+     *                                    chain against. A pin is a hex-encoded hash of a X.509 certificate's
+     *                                    SubjectPublicKeyInfo. A pin can be generated using the provided pin.py
+     *                                    script: python ./tools/pin.py certificate_file.pem
+     * @param enforceUntilTimestampMillis A timestamp (in milliseconds) when pins will stop being
+     *                                    enforced.  Normal non-pinned certificate validation
+     *                                    will continue.  Set this to some period after your build
+     *                                    date, or to 0 to enforce pins forever.
+     */
+    public PinningTrustManager(SystemKeyStore keyStore, String[] pins, long enforceUntilTimestampMillis) {
+        this.systemTrustManagers = initializeSystemTrustManagers(keyStore);
+        this.systemKeyStore = keyStore;
+        this.enforceUntilTimestampMillis = enforceUntilTimestampMillis;
 
-    for (String pin : pins) {
-      this.pins.add(hexStringToByteArray(pin));
-    }
-  }
-
-  private TrustManager[] initializeSystemTrustManagers(SystemKeyStore keyStore) {
-    try {
-      final TrustManagerFactory tmf = TrustManagerFactory.getInstance("X509");
-      tmf.init(keyStore.trustStore);
-
-      return tmf.getTrustManagers();
-    } catch (NoSuchAlgorithmException nsae) {
-      throw new AssertionError(nsae);
-    } catch (KeyStoreException e) {
-      throw new AssertionError(e);
-    }
-  }
-
-  private boolean isValidPin(X509Certificate certificate) throws CertificateException {
-    try {
-      final MessageDigest digest = MessageDigest.getInstance("SHA-256");
-      final byte[] spki          = certificate.getPublicKey().getEncoded();
-      final byte[] pin           = digest.digest(spki);
-
-      for (byte[] validPin : this.pins) {
-        if (Arrays.equals(validPin, pin)) {
-          return true;
+        for (String pin : pins) {
+            this.pins.add(hexStringToByteArray(pin));
         }
-      }
-
-      return false;
-    } catch (NoSuchAlgorithmException nsae) {
-      throw new CertificateException(nsae);
-    }
-  }
-
-  private void checkSystemTrust(X509Certificate[] chain, String authType)
-      throws CertificateException {
-    for (TrustManager systemTrustManager : systemTrustManagers) {
-      ((X509TrustManager) systemTrustManager).checkServerTrusted(chain, authType);
-    }
-  }
-
-  private void checkPinTrust(X509Certificate[] chain)
-      throws CertificateException {
-
-    if (enforceUntilTimestampMillis != 0 &&
-        System.currentTimeMillis() > enforceUntilTimestampMillis)
-    {
-      Log.w("PinningTrustManager", "Certificate pins are stale, falling back to system trust.");
-      return;
     }
 
-    final X509Certificate[] cleanChain = CertificateChainCleaner.getCleanChain(chain, systemKeyStore);
+    private TrustManager[] initializeSystemTrustManagers(SystemKeyStore keyStore) {
+        try {
+            final TrustManagerFactory tmf = TrustManagerFactory.getInstance("X509");
+            tmf.init(keyStore.trustStore);
 
-    for (X509Certificate certificate : cleanChain) {
-      if (isValidPin(certificate)) {
-        return;
-      }
+            return tmf.getTrustManagers();
+        } catch (NoSuchAlgorithmException nsae) {
+            throw new AssertionError(nsae);
+        } catch (KeyStoreException e) {
+            throw new AssertionError(e);
+        }
     }
 
-    throw new CertificateException("No valid pins found in chain!");
-  }
+    private boolean isValidPin(X509Certificate certificate) throws CertificateException {
+        try {
+            final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            final byte[] spki = certificate.getPublicKey().getEncoded();
+            final byte[] pin = digest.digest(spki);
 
-  public void checkClientTrusted(X509Certificate[] chain, String authType)
-      throws CertificateException {
-    throw new CertificateException("Client certificates not supported!");
-  }
+            for (byte[] validPin : this.pins) {
+                if (Arrays.equals(validPin, pin)) {
+                    return true;
+                }
+            }
 
-  public void checkServerTrusted(X509Certificate[] chain, String authType)
-      throws CertificateException
-  {
-    if (cache.contains(chain[0])) {
-      return;
+            return false;
+        } catch (NoSuchAlgorithmException nsae) {
+            throw new CertificateException(nsae);
+        }
     }
 
-    // Note: We do this so that we'll never be doing worse than the default
-    // system validation.  It's duplicate work, however, and can be factored
-    // out if we make the verification below more complete.
-    checkSystemTrust(chain, authType);
-    checkPinTrust(chain);
-    cache.add(chain[0]);
-  }
-
-  public X509Certificate[] getAcceptedIssuers() {
-    return null;
-  }
-
-  private byte[] hexStringToByteArray(String s) {
-    final int len = s.length();
-    final byte[] data = new byte[len / 2];
-
-    for (int i = 0; i < len; i += 2) {
-      data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) +
-          Character.digit(s.charAt(i + 1), 16));
+    private void checkSystemTrust(X509Certificate[] chain, String authType)
+            throws CertificateException {
+        try {
+            for (TrustManager systemTrustManager : systemTrustManagers) {
+                ((X509TrustManager) systemTrustManager).checkServerTrusted(chain, authType);
+            }
+        } catch (CertificateException e) {
+            throw new CertificateException(printCertificates(chain), e);
+        }
     }
 
-    return data;
-  }
+    private void checkPinTrust(X509Certificate[] chain)
+            throws CertificateException {
 
-  public void clearCache() {
-    cache.clear();
-  }
+        try {
+            if (enforceUntilTimestampMillis != 0 &&
+                    System.currentTimeMillis() > enforceUntilTimestampMillis) {
+                Log.w("PinningTrustManager", "Certificate pins are stale, falling back to system trust.");
+                return;
+            }
+
+            final X509Certificate[] cleanChain = CertificateChainCleaner.getCleanChain(chain, systemKeyStore);
+
+            for (X509Certificate certificate : cleanChain) {
+                if (isValidPin(certificate)) {
+                    return;
+                }
+            }
+
+            throw new CertificateException("No valid pins found in chain!");
+        } catch (CertificateException e) {
+            throw new CertificateException(printCertificates(chain), e);
+        }
+    }
+
+    public void checkClientTrusted(X509Certificate[] chain, String authType)
+            throws CertificateException {
+        throw new CertificateException("Client certificates not supported!");
+    }
+
+    public void checkServerTrusted(X509Certificate[] chain, String authType)
+            throws CertificateException {
+        if (cache.contains(chain[0])) {
+            return;
+        }
+
+        // Note: We do this so that we'll never be doing worse than the default
+        // system validation.  It's duplicate work, however, and can be factored
+        // out if we make the verification below more complete.
+        checkSystemTrust(chain, authType);
+        checkPinTrust(chain);
+        cache.add(chain[0]);
+    }
+
+    public X509Certificate[] getAcceptedIssuers() {
+        return null;
+    }
+
+    private byte[] hexStringToByteArray(String s) {
+        final int len = s.length();
+        final byte[] data = new byte[len / 2];
+
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) +
+                    Character.digit(s.charAt(i + 1), 16));
+        }
+
+        return data;
+    }
+
+    public void clearCache() {
+        cache.clear();
+    }
+
+    private String printCertificates(X509Certificate[] chain) {
+        String certificates = "";
+        if (chain == null) {
+            return certificates;
+        }
+
+        for (X509Certificate x509Certificate : chain) {
+            certificates += x509Certificate.toString() + "\n";
+        }
+
+        return certificates;
+    }
 }
